1fad8680825a07ef757354b16599016fd87cb81f
Santi <canosantiago404@gmail.com>
2024-06-27 20:10:18 -0300

    añadido contador y box total

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,31 @@
 import React from "react";
 
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstName: "Santi" };
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Bienvenido"
+            user={loggedIn?.firstName || "Invitado"}
+            subtext="Ingresa y maneja tu cuenta bancaria"
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
---
+++ components/TotalBalanceBox.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import CountUp from "react-countup";
+
+import { formatAmount } from "@/lib/utils";
+
+import AnimatedCounter from "./AnimatedCounter";
+
+const TotalBalanceBox = ({
+  accounts = [],
+  totalBanks,
+  totalCurrentBalance,
+}: TotlaBalanceBoxProps) => {
+  return (
+    <section className="total-balance">
+      <div className="total-balance-chart">{/* <Chart /> */}</div>
+
+      <div className="flex flex-col gap-6">
+        <h2 className="header-2">Cuentas bancarias: {totalBanks}</h2>
+        <div className="flex flex-col gap-2">
+          <p className="total-balance-label">Sueldo total</p>
+          <p className="total-balance-amount flex-center gap-2">
+            <AnimatedCounter amount={totalCurrentBalance} />
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default TotalBalanceBox;
